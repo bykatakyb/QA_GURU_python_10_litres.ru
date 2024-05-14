4d053b557a7f29df39563e748fedac362aa2bf7d
Vladimir Bukatov <bukatovvs@gmail.com>
2024-05-14 13:34:03 +0400

    try to fix

--- project_diploma/utils/attach.py
+++ project_diploma/utils/attach.py
@@ -11,6 +11,7 @@ def screenshot():
     )
 
 
+
 def add_logs(browser):
     log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
     allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
