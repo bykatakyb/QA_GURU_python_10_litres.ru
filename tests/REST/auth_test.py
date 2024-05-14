b8ac3465a5f86efbfd7e880a7fbb6fa575a4eb24
Vladimir Bukatov <bukatovvs@gmail.com>
2024-05-14 12:46:53 +0400

    add more REST API tests

--- tests/REST/auth_test.py
+++ tests/REST/auth_test.py
@@ -4,21 +4,35 @@ from allure_commons.types import Severity
 
 from project_diploma.schemes.scheme_loader import load_scheme
 from project_diploma.utils.requests_api import api_post
-from tests.REST.conftest import email, password
+from tests.REST.conftest import email, password, wrong_password
 
 
 @allure.epic('Auth')
 @allure.label("owner", "Bykat")
-@allure.feature("Authorization - POSITIVE case")
+@allure.feature("Authorization via REST - POSITIVE case")
 @allure.tag('API')
 @allure.severity(Severity.BLOCKER)
 @allure.label('layer', 'REST')
-def test_auth_for_registered_customer():
-    scheme = load_scheme('auth.json')
+def test_auth_with_right_pass_via_rest():
+    scheme = load_scheme('auth_post_pass.json')
     url = "/auth/login"
-
     result = api_post(url, json={"login": email, "password": password})
-
     assert result.status_code == 200
     jsonschema.validate(result.json(), scheme)
     assert result.json()['error'] is None
+
+
+@allure.epic('Auth')
+@allure.label("owner", "Bykat")
+@allure.feature("Authorization via REST - NEGATIVE case")
+@allure.tag('API')
+@allure.severity(Severity.NORMAL)
+@allure.label('layer', 'REST')
+def test_auth_with_wrong_pass():
+    scheme = load_scheme('auth_post_fail.json')
+    url = "/auth/login"
+    result = api_post(url, json={"login": email, "password": wrong_password})
+    assert result.status_code == 401
+    jsonschema.validate(result.json(), scheme)
+    assert result.json()['error']['type'] == "Unauthorized"
+    assert result.json()['error']['title'] == "Incorrect user data"
---
+++ tests/REST/product_search_test.py
@@ -0,0 +1,41 @@
+import allure
+import jsonschema
+from allure_commons.types import Severity
+
+from project_diploma.schemes.scheme_loader import load_scheme
+from project_diploma.utils.requests_api import api_get
+
+
+@allure.epic('Product Search')
+@allure.label("owner", "Bykat")
+@allure.feature("Product Search by title via REST - POSITIVE case")
+@allure.tag('API')
+@allure.severity(Severity.CRITICAL)
+@allure.label('layer', 'REST')
+def test_product_search_by_existed_title():
+    scheme = load_scheme('product_search_get_pass.json')
+    book_title = 'Разговоры с друзьями'
+    art_types = 'text_book'
+    types = 'text_book'
+    url = f'/search'
+    params = {"q": book_title, "art_types": art_types, "types": types}
+    result = api_get(url, params=params)
+    assert result.status_code == 200
+    assert result.json()['payload']['data'][0]['type'] == "text_book"
+
+@allure.epic('Product Search')
+@allure.label("owner", "Bykat")
+@allure.feature("Product Search by title via REST - NEGATIVE case")
+@allure.tag('API')
+@allure.severity(Severity.CRITICAL)
+@allure.label('layer', 'REST')
+def test_product_search_by_nonexistent_title():
+    scheme = load_scheme('product_search_get_fail.json')
+    book_title = 'йцукенфывап'
+    types = 'text_book'
+    url = f'/search'
+    params = {"q": book_title, "types": types}
+    result = api_get(url, params=params)
+    assert result.status_code == 200
+    jsonschema.validate(result.json(), scheme)
+    assert len(result.json()['payload']['data']) == 0
